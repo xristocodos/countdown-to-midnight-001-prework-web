f9d8e3be59e25b1401be35622f2b169501d975b8
xristocodos <xristojobos@gmail.com>
2016-01-18 17:38:05 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(qtyTimes = 20)
+  counter = qtyTimes
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(qtyTimes = 20)
+  counter = qtyTimes
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep(1)
+  end
+
+  return "HAPPY NEW YEAR!"
 end
